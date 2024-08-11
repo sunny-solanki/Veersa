efe84b25c739e0d92e67a92919518020bda7de08
kunal-216 <kunaltalreja216@gmail.com>
2024-08-11 18:41:50 +0530

    first commit

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { registerUser, loginUser } from '../controllers/authControllers.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/signup', registerUser);
-router.post('/login', loginUser);
+router.post('/signup',protect, registerUser);
+router.post('/login',protect, loginUser);
 
 export default router;
